519aacd00176db5eba5ae853830dc57ed6733d0f
lcy8417 <77670414+lcy8417@users.noreply.github.com>
2021-05-05 23:56:42 +0900

    Add files via upload

---
+++ restapi_test.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, make_response, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/test", methods=['GET', 'POST', 'PUT', 'DELETE'])
+def test():
+    if request.method == 'POST':
+        print('POST')
+        data = request.get_json()
+        print(data)
+        print(data['email'])
+    if request.method == 'GET':
+        print('GET')
+        user = request.args.get('email')
+        print(user)
+    if request.method == 'PUT':
+        print('PUT')
+        user = request.args.get('email')
+        print(user)
+    if request.method == 'DELETE':
+        print('DELETE')
+        user = request.args.get('email')
+        print(user)
+
+    return make_response(jsonify({'status': True}), 200)
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port="8082")
